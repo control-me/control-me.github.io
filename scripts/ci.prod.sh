10f58de44115fb28eec44f24cc44f510f4ed76cb
Máté Homolya <mate.homolya@gmail.com>
2021-08-03 17:10:20 -0600

    Test CI

---
+++ scripts/ci.prod.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+# https://vitejs.dev/guide/static-deploy.html#github-pages
+
+# abort on errors
+set -e
+
+# install deps
+npm ci
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:control-me/control-me.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
